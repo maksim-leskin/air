e755ffc160aa935e778bc5ba0c774416a8a9f648
Максим <maksim.leskin@methed.ru>
2022-02-01 20:33:48 +0300

    init

---
+++ scripts/modules/createElement.js
@@ -0,0 +1,10 @@
+const createElement = (tag, attribute) => {
+  const element = document.createElement(tag);
+  Object.assign(element, attribute);
+
+  return element;
+};
+
+export default createElement;
+
+
